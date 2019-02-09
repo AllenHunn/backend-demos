1c9a872e6aa6ab550ed8961b6df0068b83e2c3f3
AllenHunn <allenhunn@gmail.com>
2019-02-09 17:53:29 -0500

    Fastify Start

--- express-js/routes/cat.js
+++ express-js/routes/cat.js
@@ -2,13 +2,48 @@ const express = require('express');
 const router = express.Router();
 const catService = require('../services/cat.service');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+const validationHandler = next => result => {
+  if (result.isEmpty()) return
+  if (!next)
+    throw new Error(
+      result.array().map(i => `'${i.param}' has ${i.msg}`).join(' ')
+    )
+else
+  return next(
+    new Error(
+      result.array().map(i => `'${i.param}' has ${i.msg}`).join('')
+    )
+  )
+}
+
+router.get('/', (req, res, next) => {
   catService
     .findAll()
       .then((cats) => {
-        res.send(cats);
+        res.json(cats);
       });
 });
+
+router.get('/:id', (req, res, next) => {
+  catService
+    .findOne(req.params.id)
+      .then((cat) => {
+        res.json(cat);
+      });
+});
+
+router.post('/', catService.validateCreate(), (req, res, next) => {
+  req
+    .getValidationResult()
+    .then(validationHandler())
+    .then(() => {
+      catService
+      .create(req.body)
+        .then((cat) => res.json(cat));
+    })
+    .catch(next);
+});
+
+
 
 module.exports = router;
--- express-js/services/cat.service.js
+++ express-js/services/cat.service.js
@@ -1,7 +1,21 @@
 const Cat = require('../models/cat.model')
+const { body } = require('express-validator/check')
 
 module.exports = {
     findAll: async () => {
         return await Cat.findAll();
+    },
+    findOne: async (id) => {
+        return await Cat.findById(id);
+    },
+    create: async (createCatDto) => {
+        return await Cat.create(createCatDto);
+    },
+    validateCreate: () => {
+        return [
+            body('name', 'Must provide name').exists().not().isEmpty(),
+            body('age', 'age must be a number').isInt(),
+            body('color', 'must be a valid color').isIn(['Orange', 'Grey', 'Black', 'White'])
+        ];
     }
 }
